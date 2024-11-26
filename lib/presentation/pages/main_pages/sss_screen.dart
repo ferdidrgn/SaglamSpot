3f40271bb61eae9e71f7f2651a7057b0741514b9
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-11-26 04:50:37 +0300

    Update sss_screen.dart

--- lib/presentation/pages/main_pages/sss_screen.dart
+++ lib/presentation/pages/main_pages/sss_screen.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import '../add_product_page.dart';
 
 class SSSScreen extends StatelessWidget {
   const SSSScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('SSS', style: TextStyle(fontSize: 24)),
+    return Scaffold(
+      appBar: AppBar(title: const Text('SSS (Sıkça Sorulan Sorular)')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Sıkça Sorulan Sorular",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            const Text("1. Ürün nasıl eklenir?"),
+            const Text("Cevap: Ürün eklemek için 'Ürün Ekle' butonuna tıklayın."),
+            const SizedBox(height: 16),
+            const Text("2. Görsel nasıl yüklenir?"),
+            const Text("Cevap: Görsel seçmek için 'Görsel Seç' butonuna tıklayın."),
+            const SizedBox(height: 16),
+            const Text("3. Ürün satıldı mı? Nasıl belirlenir?"),
+            const Text("Cevap: 'Ürün Satıldı mı?' kutucuğuna tıklayın."),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                // AddProductPage sayfasına yönlendirme
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AddProductPage()),
+                );
+              },
+              child: const Text('Ürün Ekle Sayfasına Git'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
