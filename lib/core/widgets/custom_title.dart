01c24ae4edb762b7d123abd3e35231206b93e0bf
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-12-14 05:59:26 +0300

    Update custom_title.dart

--- lib/core/widgets/custom_title.dart
+++ lib/core/widgets/custom_title.dart
@@ -3,26 +3,31 @@ import 'package:flutter/material.dart';
 class CustomSectionTitle extends StatelessWidget {
   final String title;
   final double fontSize;
+  final Color color;
 
   // Constructor
   const CustomSectionTitle({
     super.key,
     required this.title,
     this.fontSize = 24,
+    this.color = Colors.black, // Varsayılan renk
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-        child: Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: fontSize,
-                fontWeight: FontWeight.bold,
-              ),
-            )));
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: FontWeight.bold,
+            color: color, // Renk özelliği ekleniyor
+          ),
+        ),
+      ),
+    );
   }
 }
