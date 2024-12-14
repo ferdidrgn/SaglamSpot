ff977aea5c0ce67ef7b510a5d2b0e9f8a15b9223
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-12-14 06:35:13 +0300

    Update product_repository.dart

--- lib/domain/repositories/product_repository.dart
+++ lib/domain/repositories/product_repository.dart
@@ -5,14 +5,12 @@ import '../entities/product.dart';
 abstract class ProductRepository {
   /// Get a list of products.
   Future<Either<Failure, List<Product>>> getProducts();
-
   /// Get a list of products filtered by optional parameters.
   Future<Either<Failure, List<Product>>> getFilteredProducts({
     String? condition,
     double? minPrice,
     double? maxPrice,
   });
-
   /// Add a new product with associated images.
   Future<Either<Failure, void>> addProduct(Product product, List<String> images);
 }
