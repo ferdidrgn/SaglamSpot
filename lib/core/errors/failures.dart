724cf6d019f17a79031a295601d779337a068de5
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-12-14 06:10:36 +0300

    Update failures.dart

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -2,10 +2,11 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
   final String message;
+
   const Failure(this.message);
 
   @override
-  List<Object> get props => [message];
+  List<Object?> get props => [message];
 }
 
 class ServerFailure extends Failure {
@@ -18,4 +19,4 @@ class CacheFailure extends Failure {
 
 class NetworkFailure extends Failure {
   const NetworkFailure(super.message);
-} 
+}
