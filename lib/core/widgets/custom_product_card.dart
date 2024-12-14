7afeccb7f0c0af6dafae98f7f34b5eeedaf4d547
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-12-14 05:59:18 +0300

    Update custom_product_card.dart

--- lib/core/widgets/custom_product_card.dart
+++ lib/core/widgets/custom_product_card.dart
@@ -1,5 +1,5 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:saglamspot/domain/entities/product.dart';
 
 class ProductCard extends StatefulWidget {
@@ -19,7 +19,7 @@ class _ProductCardState extends State<ProductCard> {
     final product = widget.product;
 
     return SizedBox(
-      width: 280, // Yatay genişlik
+      width: 280,
       child: Card(
         elevation: 10,
         margin: const EdgeInsets.symmetric(horizontal: 8),
