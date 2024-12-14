0fc5614f7dd464d429af3e06ac4229da18e866f2
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-12-14 06:02:35 +0300

    Update custom_search.dart

--- lib/core/widgets/custom_search.dart
+++ lib/core/widgets/custom_search.dart
@@ -15,60 +15,60 @@ class CustomSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 5,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: TextField(
-          controller: controller,
-          decoration: InputDecoration(
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Theme.of(context).brightness == Brightness.light
-                    ? Colors.red.shade200
-                    : const Color(0xFFCF6679),
-                width: 3,
-              ),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Theme.of(context).brightness == Brightness.light
-                    ? Colors.red.shade500
-                    : Colors.red.shade200,
-                width: 3,
-              ),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            hintText: 'Eşya Ara...',
-            hintStyle: TextStyle(
-              fontSize: 18,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w500,
-              color: Theme.of(context).colorScheme.onSurface,
-            ),
-            contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
-            prefixIcon: IconButton(
-              icon: Icon(
-                  Icons.youtube_searched_for,
-                  color: Theme.of(context).colorScheme.error
-              ),
-              onPressed: onSearchTap,
-            ),
-            suffixIcon: controller?.text.isNotEmpty ?? false ? IconButton(
-              icon: Icon(
-                Icons.clear,
-                color: Theme.of(context).colorScheme.error,
-              ),
-              onPressed: () {
-                controller?.clear();
-                onSearchChanged?.call('');
-              },
-            ) : null,
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          enabledBorder: _buildBorder(context, false),
+          focusedBorder: _buildBorder(context, true),
+          hintText: 'Eşya Ara...',
+          hintStyle: TextStyle(
+            fontSize: 18,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.w500,
+            color: Theme.of(context).colorScheme.onSurface,
           ),
-          onTap: onSearchTap,
-          onChanged: onSearchChanged,
-        )
+          contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 15),
+          prefixIcon: IconButton(
+            icon: Icon(
+              Icons.search, // Arama ikonu
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            onPressed: onSearchTap,
+          ),
+          suffixIcon: controller?.text.isNotEmpty ?? false
+              ? IconButton(
+                  icon: Icon(
+                    Icons.clear,
+                    color: Theme.of(context).colorScheme.error,
+                  ),
+                  onPressed: () {
+                    controller?.clear();
+                    onSearchChanged?.call('');
+                  },
+                )
+              : null,
+        ),
+        onTap: onSearchTap,
+        onChanged: onSearchChanged,
+      ),
+    );
+  }
+
+  OutlineInputBorder _buildBorder(BuildContext context, bool isFocused) {
+    Color borderColor = Theme.of(context).brightness == Brightness.light
+        ? (isFocused ? Colors.red.shade500 : Colors.red.shade200)
+        : (isFocused ? Colors.red.shade200 : const Color(0xFFCF6679));
+
+    return OutlineInputBorder(
+      borderSide: BorderSide(
+        color: borderColor,
+        width: 3,
+      ),
+      borderRadius: BorderRadius.circular(10),
     );
   }
 }
