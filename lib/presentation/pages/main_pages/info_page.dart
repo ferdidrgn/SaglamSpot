b7c51dd0e62a7ee1f975e7d51984e058cca8c289
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-12-15 06:26:42 +0300

    Update info_page.dart

--- lib/presentation/pages/main_pages/info_page.dart
+++ lib/presentation/pages/main_pages/info_page.dart
@@ -17,8 +17,7 @@ class InfoPage extends StatelessWidget {
             _buildDecoratedCard(
               context,
               title: 'İş Yeri Bilgileri',
-              content:
-                  'Müşterilerimize en kaliteli ve şık mobilya çözümleri sunarak, yaşam alanlarını daha konforlu ve estetik hale getiriyoruz. Her bir ürünümüz, zarafet ve işlevselliği bir araya getirerek, evlerinizi ve ofislerinizi hayalinizdeki mekanlara dönüştürmeyi hedefliyor. Bizimle, sadece bir alışveriş değil, aynı zamanda bir yaşam tarzı deneyimi yaşıyorsunuz.',
+              content: 'Müşterilerimize en kaliteli ve şık mobilya çözümleri sunarak, yaşam alanlarını daha konforlu ve estetik hale getiriyoruz. Her bir ürünümüz, zarafet ve işlevselliği bir araya getirerek, evlerinizi ve ofislerinizi hayalinizdeki mekanlara dönüştürmeyi hedefliyor. Bizimle, sadece bir alışveriş değil, aynı zamanda bir yaşam tarzı deneyimi yaşıyorsunuz.',
               color: Colors.brown[200]!,
               imageUrl: 'assets/images/furniture_shop.png',
             ),
