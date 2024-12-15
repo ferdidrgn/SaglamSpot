a9fc0b0a553598a621982bcde8ae0844c797433e
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-12-16 02:59:54 +0300

    add use cases
    add usecase, update usecase, delete usecase

---
+++ lib/domain/usecases/add_product_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failures.dart';
+import '../entities/product.dart';
+import '../repositories/product_repository.dart';
+
+abstract class AddProductUseCase {
+  Future<Either<Failure, void>> call(Product product, List<String> images);
+}
+
+class AddProductUseCaseImpl implements AddProductUseCase {
+  final ProductRepository repository;
+
+  AddProductUseCaseImpl(this.repository);
+
+  @override
+  Future<Either<Failure, void>> call(Product product, List<String> images) async {
+    return await repository.addProduct(product, images);
+  }
+} 
