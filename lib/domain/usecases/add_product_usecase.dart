6792b888b46ee8ea68d53417b0f696e6fba97928
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-12-16 04:12:56 +0300

    Update add_product_usecase.dart

--- lib/domain/usecases/add_product_usecase.dart
+++ lib/domain/usecases/add_product_usecase.dart
@@ -4,7 +4,7 @@ import '../entities/product.dart';
 import '../repositories/product_repository.dart';
 
 abstract class AddProductUseCase {
-  Future<Either<Failure, void>> call(Product product, List<String> images);
+  Future<Either<Failure, void>> call(Product product, List<dynamic> images);
 }
 
 class AddProductUseCaseImpl implements AddProductUseCase {
@@ -13,7 +13,7 @@ class AddProductUseCaseImpl implements AddProductUseCase {
   AddProductUseCaseImpl(this.repository);
 
   @override
-  Future<Either<Failure, void>> call(Product product, List<String> images) async {
+  Future<Either<Failure, void>> call(Product product, List<dynamic> images) async {
     return await repository.addProduct(product, images);
   }
 } 
