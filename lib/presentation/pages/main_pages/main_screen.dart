f3cc42e5c5f87f9e5c1ac7724b57c230e0f91e5e
Ferdi Durgun <ferdidurgun34@gmail.com>
2025-11-10 22:34:07 +0300

    Update main_screen.dart

--- lib/presentation/pages/main_pages/main_screen.dart
+++ lib/presentation/pages/main_pages/main_screen.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
-import '../../../core/widgets/app_header.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/theme/app_colors.dart';
+import '../../../core/widgets/modern_app_header.dart';
 import 'home_page.dart';
 import 'info_page.dart';
 import 'new_products_page.dart';
 import 'spot_products_page.dart';
 import 'sss_page.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends ConsumerStatefulWidget {
   const MainScreen({super.key});
 
   @override
-  _MainScreenState createState() => _MainScreenState();
+  ConsumerState<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends ConsumerState<MainScreen> {
   int _selectedIndex = 0;
 
-  // List of pages
   final List<Widget> _pages = const [
     HomePage(),
     NewProductsPage(),
@@ -34,21 +35,41 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(final BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // App Header
-          AppHeader(
-            selectedIndex: _selectedIndex,
-            onItemSelected: _onItemSelected,
-          ),
-          // Page content starts below the App Header
-          Expanded(
-            child: IndexedStack(
-              index: _selectedIndex,
-              children: _pages,
+      backgroundColor: AppColors.background,
+      body: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        decoration: const BoxDecoration(
+          gradient: AppColors.backgroundGradient,
+        ),
+        child: Column(
+          children: [
+            // Modern App Header
+            ModernAppHeader(
+              selectedIndex: _selectedIndex,
+              onItemSelected: _onItemSelected,
             ),
-          ),
-        ],
+
+            // Page Content
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      AppColors.background.withOpacity(0.8),
+                      AppColors.background.withOpacity(0.4),
+                    ],
+                  ),
+                ),
+                child: IndexedStack(
+                  index: _selectedIndex,
+                  children: _pages,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
