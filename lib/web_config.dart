891b0fe65c27f5c4b9f24c80afce1a1d97f14e5b
Ferdi Durgun <ferdidurgun34@gmail.com>
2025-11-27 02:48:05 +0300

    update

---
+++ lib/web_config.dart
@@ -0,0 +1,7 @@
+const Map<String, String> firebaseConfig = {
+  'apiKey': 'AIzaSyCYZG-PkPWxp2PMahl1nEN0GwUmIqlLf2I',
+  'appId': '1:257014284345:web:2092e3dd24aae5f022cde8',
+  'messagingSenderId': '257014284345',
+  'projectId': 'saglamspotflutter-2a1a8',
+  'storageBucket': 'saglamspotflutter-2a1a8.firebasestorage.app',
+};
