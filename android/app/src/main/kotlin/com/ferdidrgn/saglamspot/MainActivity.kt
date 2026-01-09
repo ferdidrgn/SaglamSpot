81e9cfa39e6711b7408a22bc29019e2c72123c2a
Ferdi Durgun <ferdidurgun34@gmail.com>
2026-01-10 02:08:39 +0300

    update

--- android/app/src/main/kotlin/com/ferdidrgn/saglamspot/MainActivity.kt
+++ android/app/src/main/kotlin/com/ferdidrgn/saglamspot/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.ferdidrgn.saglamspot
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import androidx.core.view.WindowCompat
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Flutter yerleşmeden önce ekran ayarını yap
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+    }
+}
