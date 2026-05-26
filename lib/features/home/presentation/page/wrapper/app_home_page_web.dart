a95aafdff215adf69fb571e1d4c782248dd99c55
Ferdi Durgun <ferdidurgun34@gmail.com>
2026-05-26 15:21:19 +0300

    update home page

--- lib/features/home/presentation/page/wrapper/app_home_page_web.dart
+++ lib/features/home/presentation/page/wrapper/app_home_page_web.dart
@@ -1,27 +1,11 @@
 import 'package:flutter/material.dart';
 import '../../../../../shared/navigation/providers/navigation_keys.dart';
-import '../../widgets/architectural_editorial_showroom.dart';
 import '../home_page_web.dart';
 
 class AppHomePage extends StatelessWidget {
   const AppHomePage({super.key});
 
   @override
-  Widget build(final BuildContext context) => Scaffold(
-        backgroundColor: const Color(0xFFF8FAFC),
-        body: SafeArea(
-          top: false,
-          child: CustomScrollView(
-            physics: const BouncingScrollPhysics(),
-            slivers: [
-              // 1. Web arayüzü için de lüks editoryal showroom vitrini en tepede
-              const SliverToBoxAdapter(child: ArchitecturalEditorialShowroom()),
-
-              // 2. Sizin orijinal web ana sayfa içeriğiniz (Key korunarak çağrıldı)
-              SliverToBoxAdapter(
-                  child: HomePage(key: NavigationKeys.webNavKey)),
-            ],
-          ),
-        ),
-      );
+  Widget build(final BuildContext context) =>
+      HomePage(key: NavigationKeys.webNavKey);
 }
